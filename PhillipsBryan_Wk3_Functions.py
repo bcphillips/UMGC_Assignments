0b5a4c40016ecf81b3d2751fcf7e3b566055339f
bcphillips <142845799+bcphillips@users.noreply.github.com>
2023-08-25 16:44:30 -0400

    DATA 300 Week 3 assignment

--- PhillipsBryan_Wk3_Functions.py
+++ PhillipsBryan_Wk3_Functions.py
@@ -10,13 +10,38 @@ DATA 300/6381
 
 """
 
+# Part #1
+# ----------------------------------------------------------------------------------------------------------------------
 
-# Function fahrToCelsius converts a temperature in Fahrenheit to degrees Celsius.
-# It contains one input parameter called tempFahrenheit and returns the variable convertedTemp back to the user.
+
 def fahrToCelsius(tempFahrenheit):
+    # Function fahrToCelsius converts a temperature in Fahrenheit to degrees Celsius.
+    # It contains one input parameter called tempFahrenheit and returns the variable convertedTemp back to the user.
+
     converterTemp = (tempFahrenheit - 32) * 5 / 9
     return converterTemp
 
 
-# Test Case - Update number between parenthesis in order to convert Fahrenheit temperature to degrees Celsius
+# Test Case: Update number between parenthesis in order to convert Fahrenheit temperature to degrees Celsius
 print("32 degrees Fahrenheit in Celsius is: ", fahrToCelsius(32))
+
+# Part #2
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def tempClassifier(tempCelsius):
+
+    if tempCelsius < -2:
+        return 0, "cold"
+    elif -2 <= tempCelsius < 2:
+        return 1, "slippery"
+    elif 2 <= tempCelsius < 15:
+        return 2, "comfortable"
+    else:
+        return 3, "warm"
+
+
+user_temperature = fahrToCelsius(32)
+
+print("\r")  # Carriage return new line
+print(tempClassifier(user_temperature))
