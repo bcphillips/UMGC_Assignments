87869c645cdab6a51729771b82c330bf111777c2
bcphillips <142845799+bcphillips@users.noreply.github.com>
2023-08-29 10:01:06 -0400

    DATA 300 Week 3 assignment

--- PhillipsBryan_Wk2_Datatypes.py
+++ PhillipsBryan_Wk2_Datatypes.py
@@ -10,10 +10,13 @@ DATA 300/6381
 list3 = list(('hello', 1, 'you', 6.3, 'yes', 7, 2.3))
 
 # For loop that iterates through list3
+
 for i in range(len(list3)):
+
     # if statement to check if the list element is type str
     if str(type(list3[i])) == "<class 'str'>":
         list3[i] = list3[i] + " of course"
+
     # elif statement to check if the list element is type float
     elif str(type(list3[i])) == "<class 'float'>":
         # Add 5 to the original values of float in list3
