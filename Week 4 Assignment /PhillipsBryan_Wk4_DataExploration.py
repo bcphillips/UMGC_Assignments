f8f338a2c64f11437b14aad334b0dcf935a760a5
bcphillips <142845799+bcphillips@users.noreply.github.com>
2023-09-05 18:14:23 -0400

    DATA 300 Week 3 assignment

--- Week 4 Assignment /PhillipsBryan_Wk4_DataExploration.py
+++ Week 4 Assignment /PhillipsBryan_Wk4_DataExploration.py
@@ -54,7 +54,6 @@ print("How many unique stations exists in the data? (USAF Column): ", uni_Column
 # Part 2
 # ----------------------------------------------------------------------------------------------------------------------
 # Create variable using data from only columns USAF, YR--MODAHRMN, TEMP, MAX & MIN
-print("\r")  # Carriage return new line
 selected = data[["USAF", "YR--MODAHRMN", "TEMP", "MAX", "MIN"]]
 
 # Use dropna() function to clean selected columns of N/A data and create nondestructive copy
@@ -80,3 +79,45 @@ kumpula.to_csv("Kumpula_temps_May_Aug_2017.csv", index=False, float_format="%.1f
 
 # Save Rovaniemi data to new .csv file separated with commas, and one decimal point floating point numbers
 rovaniemi.to_csv("Rovaniemi_temps_May_Aug_2017.csv", index=False, float_format="%.1f", sep=",")
+
+
+# Part 4a
+# ----------------------------------------------------------------------------------------------------------------------
+# Create variable for new DataFrame containing data from Kumpula_temps_May_Aug_2017.csv file
+kumpula_data = pd.read_csv("Kumpula_temps_May_Aug_2017.csv")
+
+# Create variable for new DataFrame containing data from Rovaniemi_temps_May_Aug_2017.csv file
+rovaniemi_data = pd.read_csv("Rovaniemi_temps_May_Aug_2017.csv")
+
+# Display the median temperature for Helsinki Kumpula in Celsius
+print("\r")  # Carriage return new line
+print("Median temperature in Helsinki Kumpula: ", kumpula_data["Celsius"].median())
+
+# Display the median temperature for Rovaniemi in Celsius
+print("\r")  # Carriage return new line
+print("Median temperature in Rovaniemi: ", rovaniemi_data["Celsius"].median())
+
+
+# Part 4b
+# ----------------------------------------------------------------------------------------------------------------------
+# Create variable to store Helsinki Kumpula & Rovaniemi May temperatures
+kumpula_may = kumpula_data.loc[(kumpula_data
+                                ["YR--MODAHRMN"] >= 201705010000) & (kumpula_data["YR--MODAHRMN"] < 201706010000)]
+
+rovaniemi_may = rovaniemi_data.loc[(rovaniemi_data
+                                    ["YR--MODAHRMN"] >= 201705010000) & (rovaniemi_data["YR--MODAHRMN"] < 201706010000)]
+
+
+# Create variable to store Helsinki Kumpula & Rovaniemi June temperatures
+kumpula_june = kumpula_data.loc[(kumpula_data
+                                 ["YR--MODAHRMN"] >= 201706010000) & (kumpula_data["YR--MODAHRMN"] < 201707010000)]
+
+rovaniemi_june = rovaniemi_data.loc[(rovaniemi_data
+                                    ["YR--MODAHRMN"] >= 201706010000) & (rovaniemi_data["YR--MODAHRMN"] < 201707010000)]
+
+# Create variable for statistical analysis of Helsinki Kumpula mean, min and max temperatures for May & June
+kumpula_may_mean = kumpula_may["Celsius"].mean().round(1)
+
+# Test output
+print("\r")  # Carriage return new line
+print(f"Helsinki Kumpula mean temperature for the month of may is {kumpula_may_mean} degrees Celsius.")
