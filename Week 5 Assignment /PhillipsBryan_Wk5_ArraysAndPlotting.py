120a100d41d7cec7f6eb8052d959d2a006bd98e3
bcphillips <142845799+bcphillips@users.noreply.github.com>
2023-09-18 18:31:29 -0400

    DATA 300 Week 3 assignment

--- Week 5 Assignment /PhillipsBryan_Wk5_ArraysAndPlotting.py
+++ Week 5 Assignment /PhillipsBryan_Wk5_ArraysAndPlotting.py
@@ -10,48 +10,73 @@ Part B - matplotlib Plotting
 """
 
 # Imports
-# ----------------------------------------------------------------------------------------------------------------------
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Set Pandas Display Options
+# Part A
 # ----------------------------------------------------------------------------------------------------------------------
+
+# Initialize 3D array with each array containing three elements
+arr3 = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+# Display the 3D array
+print(arr3)
+
+# Display the shape of the 3D array
+print(np.shape(arr3))
+
+# Three nested loops to access all the elements of the 3D array
+for ii in range(np.shape(arr3)[0]):
+    for jj in range(np.shape(arr3)[1]):
+        for kk in range(np.shape(arr3)[2]):
+            print(arr3[ii, jj, kk])  # Display all the elements
+
+
+# Part B
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Import Pandas and set display options to view all data without truncation
 pd.set_option('display.max_columns', None)  # Display all columns
 pd.set_option('display.max_rows', None)  # Display all rows
 pd.set_option('display.width', None)  # Width of the display in characters
 pd.set_option('display.max_colwidth', None)  # Display the full content of each column
 
-# Part A: Looping through 3D Array
-# ----------------------------------------------------------------------------------------------------------------------
-arr3 = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-print(arr3)
-shape = arr3.shape
-
-for ii in range(shape[0]):
-    for jj in range(shape[1]):
-        for kk in range(shape[2]):
-            print(arr3[ii, jj, kk])
-
-# Part B1: Comparison of GDP for China & India
-# ----------------------------------------------------------------------------------------------------------------------
+# Create DataFrame from .csv file and use country as index
 data = pd.read_csv("gapminder_gdp_asia.csv", index_col="country")
+
+# Create variable to store "gdpPercap_xxxx" year data and extract only year data as string
 years = data.columns.str.strip("gdpPercap_")
+
+# Select GDP data for China & India.
 gdp_china = data.loc["China"]
 gdp_india = data.loc["India"]
 
-# Plotting
+# Plot data with differently colored lines
 plt.plot(years, gdp_china, "r--", label="China")
 plt.plot(years, gdp_india, "g-", label="India")
+
+# Create title, legend, x-axis & y-axis label
 plt.title("Comparison of GDP for China & India: 1952-2007")
-plt.legend(loc="lower right")
+plt.legend(loc="lower right")  # Show legend in the lower right-hand corner of the plot
 plt.xlabel("Year")
 plt.ylabel("GDP per capita")
+
+# Display line plot
 plt.show()
 
-# Part B2: Correlation Between GDP & Life Expectancy for 2007
+# Part B2
 # ----------------------------------------------------------------------------------------------------------------------
+
+# Create DataFrame from .csv file and use country as index
 data_all = pd.read_csv("gapminder_all.csv", index_col="country")
-data_all.plot(kind='scatter', x='gdpPercap_2007', y='lifeExp_2007', s=data_all['pop_2007']/1e6)
+
+# Create scatter plot that shows the correlation between GDP and life expectancy for 2007
+data_all.plot(kind='scatter', x='gdpPercap_2007', y='lifeExp_2007',
+              s=data_all['pop_2007']/1e6)
+
+# Create title for graph/plot and display scatter plot
 plt.title("Correlation Between GDP & Life Expectancy for 2007")
+
+# Display the scatter plot
 plt.show()
